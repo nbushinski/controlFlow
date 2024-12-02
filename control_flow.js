8831c0a8a29417b157f4660284ec958e514f3912
nbushinski <nbushinski@gmail.com>
2024-12-01 23:32:24 -0500

    Update control_flow.js

--- control_flow.js
+++ control_flow.js
@@ -1,1 +1,10 @@
-
+let userRole = "admin";
+let accessLevel;
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+console.log("Access Level:", accessLevel);
